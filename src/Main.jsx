1671a1518b2284b0cadf9f63a704cfdb84d52a9f
AminTahri <amintahri02@gmail.com>
2022-03-02 09:54:20 +0100

    this is first test

---
+++ src/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <img className='logo' src="https://catalogue.automobile.tn/marques/9.png?t=2" alt="" />
+        <br />
+        <img className='photo' src="https://images.bfmtv.com/c9GXG7EKonb-O1W_zu_8CVXubuM=/0x0:1600x900/800x0/images/Groupe-Kroely-1137664.jpg" alt="" />
+            <div className='video'>
+            <iframe width="320" height="240" src="https://www.youtube.com/embed/-4zsY28t76k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+       <h1> Mercedes Benz </h1>
+    </div>
+  )
+}
+
+export default Title
